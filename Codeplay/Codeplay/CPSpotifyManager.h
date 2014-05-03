e24f6337e3292f3aa257df56943385563297346a
Chris Nordqvist <chris@chrisnordqvist.se>
2014-05-03 10:23:43 +0200

    Added Spotify Manager

---
+++ Codeplay/Codeplay/CPSpotifyManager.h
@@ -0,0 +1,14 @@
+//
+//  CPSpotifyManager.h
+//  Codeplay
+//
+//  Created by Chris Nordqvist on 2014-05-03.
+//  Copyright (c) 2014 Codeplay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Spotify/Spotify.h>
+
+@interface CPSpotifyManager : NSObject
+
+@end
---
+++ Codeplay/Codeplay/CPSpotifyManager.m
@@ -0,0 +1,13 @@
+//
+//  CPSpotifyManager.m
+//  Codeplay
+//
+//  Created by Chris Nordqvist on 2014-05-03.
+//  Copyright (c) 2014 Codeplay. All rights reserved.
+//
+
+#import "CPSpotifyManager.h"
+
+@implementation CPSpotifyManager
+
+@end
