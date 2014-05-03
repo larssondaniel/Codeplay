e21c5bedab1adb41f3e19e59b483649b721bcdd3
Daniel Larsson <daniel.larsson@ctk.se>
2014-05-03 16:59:11 +0200

    I wonder

---
+++ Codeplay/Codeplay/CPGameManager.m
@@ -0,0 +1,31 @@
+//
+//  CPGameManager.m
+//  Codeplay
+//
+//  Created by Chris Nordqvist on 2014-05-03.
+//  Copyright (c) 2014 Codeplay. All rights reserved.
+//
+
+#import "CPGameManager.h"
+#import "CPAppDelegate.h"
+
+
+@implementation CPGameManager
+
+-(id)init{
+    
+    //NSArray *allPeers =
+    
+    return self;
+}
+
+
+-(void)setupGameFromPlaylists{
+    
+    
+    
+}
+
+
+
+@end
